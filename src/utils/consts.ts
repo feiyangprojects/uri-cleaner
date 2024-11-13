d98740d63c1f214b65462e3402577c343ca86b08
Fei Yang <io@feiyang.eu.org>
2024-11-13 07:09:52 +0000

    refactor: remove Node.js support comment

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,9 +1,7 @@
 function getEnv(key: string): string | undefined {
     if (typeof Deno === "object") {
         return Deno.env.get(key)
-    // @ts-expect-error preserve compatibility with Node.js 
     } else if (typeof process === "object") {
-        // @ts-expect-error see L4
         return process.env[key]
     } else {
         return undefined
