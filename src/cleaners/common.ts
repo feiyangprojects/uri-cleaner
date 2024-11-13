1831d882547a92c8167c01b2d8018635a4a47a6e
Fei Yang <io@feiyang.eu.org>
2024-11-13 07:31:07 +0000

    refactor: send HEAD request in common cleaner

--- src/cleaners/common.ts
+++ src/cleaners/common.ts
@@ -13,7 +13,7 @@ export async function cleaner(url: URL, secure?: boolean): Promise<URL> {
         }
     }
 
-    const response = await fetch(url, { redirect: "manual" });
+    const response = await fetch(url, { method: "HEAD", redirect: "manual" });
     const header = response.headers.get("location");
 
     if (header === null) {
