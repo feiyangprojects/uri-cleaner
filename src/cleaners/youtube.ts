db7360379e419e412164e6467035805463ce6b86
Fei Yang <io@feiyang.eu.org>
2024-11-15 04:17:02 +0000

    feat: add more parameter to youtube cleaner

--- src/cleaners/youtube.ts
+++ src/cleaners/youtube.ts
@@ -2,4 +2,4 @@ export const pattern = new URLPattern(
     "http{s}?://{(m|www|music).}?youtube.com/*",
 );
 
-export const parameters = ["feature", "si"];
+export const parameters = ["feature", "pp", "si"];
